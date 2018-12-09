b2e5250fefc64a4421d410e8217a0a73f2aa2577
Taeho Lee <smartdolphin07@gmail.com>
2018-12-09 02:01:30 +0000

    Add TextImagePriceNN network
    Signed-off-by: Taeho Lee <smartdolphin07@gmail.com>

--- network.py
+++ network.py
@@ -68,7 +68,7 @@ class TextOnly:
 
 class TextImage:
     def __init__(self):
-        self.logger = get_logger('textimg')
+        self.logger = get_logger('text_img')
 
     def get_model(self, num_classes, activation='softmax'):
         max_len = opt.max_len
@@ -105,7 +105,7 @@ class TextImage:
 
 class TextImagePrice:
     def __init__(self):
-        self.logger = get_logger('textimgprice')
+        self.logger = get_logger('text_img_price')
 
     def get_model(self, num_classes, activation='softmax'):
         max_len = opt.max_len
@@ -135,6 +135,51 @@ class TextImagePrice:
         embd_out = Dropout(rate=0.5)(pair)
         relu = Activation('relu', name='relu1')(embd_out)
         outputs = Dense(num_classes, activation=activation)(relu)
+        model = Model(inputs=[t_uni, w_uni, img, price], outputs=outputs)
+        optm = keras.optimizers.Nadam(opt.lr)
+        model.compile(loss='categorical_crossentropy',
+                    optimizer=optm,
+                    metrics=[top1_acc])
+        model.summary(print_fn=lambda x: self.logger.info(x))
+        return model
+
+
+class TextImagePriceNN:
+    def __init__(self):
+        self.logger = get_logger('text_img_price_nn')
+
+    def get_model(self, num_classes, activation='softmax'):
+        max_len = opt.max_len
+        hidden_size = opt.hidden_size
+        voca_size = opt.unigram_hash_size + 1
+
+        embd = Embedding(voca_size,
+                         opt.embd_size,
+                         name='uni_embd')
+
+        t_uni = Input((max_len,), name="input_1")
+        t_uni_embd = embd(t_uni)  # token
+
+        w_uni = Input((max_len,), name="input_2")
+        w_uni_mat = Reshape((max_len, 1))(w_uni)  # weight
+
+        # image feature
+        img = Input((opt.img_size,), name="input_3")
+
+        # price feature
+        price = Input((1,), name="input_4")
+
+        text_embd = dot([t_uni_embd, w_uni_mat], axes=1)
+        text_embd = Reshape((opt.embd_size, ))(text_embd)
+        img_feat = Reshape((opt.img_size, ))(img)
+        price_feat = Reshape((1, ))(price)
+        x = concatenate([text_embd, img_feat, price_feat])
+        x = Dropout(rate=0.5)(x)
+        x = Dense(hidden_size, activation='relu')(x)
+        x = Dropout(rate=0.5)(x)
+        x = Dense(hidden_size // 2, activation='relu')(x)
+        x = Dropout(rate=0.5)(x)
+        outputs = Dense(num_classes, activation=activation)(x)
         model = Model(inputs=[t_uni, w_uni, img, price], outputs=outputs)
         optm = keras.optimizers.Nadam(opt.lr)
         model.compile(loss='categorical_crossentropy',
