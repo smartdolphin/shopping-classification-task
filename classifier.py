8a7994a608a08bed2b995b13cb5c44bb52b31190
Taeho Lee <smartdolphin07@gmail.com>
2018-12-13 22:56:27 +0000

    Update score weight to highlight the weight difference
    Signed-off-by: Taeho Lee <smartdolphin07@gmail.com>

--- classifier.py
+++ classifier.py
@@ -187,8 +187,9 @@ class Classifier():
             for k, v in inv_y_vocab.items():
                 score = sum([0 if d == -1  else s
                             for d, s  in zip(list(map(int, v.split('>'))),
-                                             [1.0, 1.2, 1.3, 1.4])]) / 4.0 * opt.factor_rate
+                                             [1.0, 1.2, 1.3, 1.4])]) / 4.0
                 class_weights.append(score)
+            class_weights = list(np.power(class_weights, opt.score_exp))
             self.logger.info('factor rate: {}'.format(opt.factor_rate))
             self.logger.info('score weight mean: {}'.format(np.mean(class_weights)))
             self.logger.info('score weight std: {}'.format(np.std(class_weights)))
